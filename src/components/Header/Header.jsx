ec846fd37ee3d5af434debb99fa80401855138ae
nilima kafle <nilimakafle2018@gmail.com>
2025-11-13 22:33:36 +0300

    Created a listener in Header.jsx to navigate home from the logo and make it look clickable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom"; //importing navigate hook
 import logo from "../../assets/PFLogo.png";
 import React from "react";
 import "./Header.css";
 
 function Header() {
+  const navigate = useNavigate(); //initilaize navigation
+
+  //click handler to go to home page
+  const handleLogoClick = () => {
+    navigate("/"); //navigate to home route
+  }
+
   return (
     <header className="header">
       {/*PF logo on the left side in navbar*/}
       <div className="header-left">
-        <img src={logo} alt="Process Feedback Logo" className="logo" />
+        <img src={logo} alt="Process Feedback Logo" className="logo"
+          onClick={handleLogoClick} //added click handler
+          style={{ cursor: "pointer" }} //making to look clickable
+        />
+
       </div>
 
       {/*Navigaion links on the right side in navbar*/}
